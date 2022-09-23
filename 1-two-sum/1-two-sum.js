41243aed9bf5c81bf2d3251596b9038a5457c444
Amine Mahjoub <31483078+eminemahjoub@users.noreply.github.com>
2022-09-23 18:07:07 +0200

    Time: 105 ms (70.76%), Space: 43.1 MB (30.93%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  var hash = {};
+  var len = nums.length;
+  for (var i = 0; i < len; i++) {
+    if (nums[i] in hash) return [hash[nums[i]], i];
+    hash[target - nums[i]] = i
+  }
+  return [-1, -1];
+};
